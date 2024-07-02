b538a62bcee8ad32aaa8909dadb813714f58e678
Ricardo Franco Delgado <rf041555@gmail.com>
2024-07-02 14:30:43 -0300

    template literals aula

---
+++ javascript-modulo-3/03-template-literals/index.js
@@ -0,0 +1,9 @@
+const name = 'Ricardo'
+console.log(`Olá, ${name}!
+Estamos em ${Date()}
+2 + 2 = ${2 + 2}`)
+
+/**
+ * Template literals serve para que possamos concatenar de forma mais prática
+ * nossas strings em javascript, caso queiramos utilizar código JS junto
+ */
