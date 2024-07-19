ed277d31cfe588f3defc01caf1acfda092161d44
Ricardo Franco Delgado <rf041555@gmail.com>
2024-07-19 17:49:57 -0300

    aula de classes e instanciação de objetos

--- javascript-modulo-4/04-classes-do-es6/Book.js
+++ javascript-modulo-4/04-classes-do-es6/Book.js
@@ -6,10 +6,21 @@ class Book {
   constructor(title) {
     // temos acesso ao elemento this dentro do constructor
     this.title = title
+    this.published = false
+  }
+
+  publish() {
+    this.published = true
   }
 }
 // Instanciação de um novo livro
 const arteDaGuerra = new Book("Sun Tzu Arte da Guerra")
 const oPoderDoHabito = new Book("O Poder do Hábito")
+
+oPoderDoHabito.publish()
+
 console.log(oPoderDoHabito)
 console.log(arteDaGuerra)
+
+// método para saber se uma instancia pertence a classe
+console.log(oPoderDoHabito instanceof Book)
