cda071635f3ee72f8f2dcf700b2df8a4fb817c1b
Ricardo Franco Delgado <rf041555@gmail.com>
2024-07-22 21:44:01 -0300

    mudanças no endereço

--- javascript-modulo-4/08-associacao/index.js
+++ javascript-modulo-4/08-associacao/index.js
@@ -2,8 +2,8 @@ const Address = require('./Address')
 const Person = require('./Person')
 
 
-const address = new Address("Av. Cesário de Mello", 1180, "Casa 2", "Paciência", "RJ")
-const ricardo = new Person("Ricardo Franco Delgado", 24, address)
+const address = new Address("Av. Endereço Endereço", 1111, "Casa", "Teste", "RJ")
+const ricardo = new Person("Ricardo ... ...", 24, address)
 
 console.log(ricardo)
 console.log(ricardo.address.fullAddress())
