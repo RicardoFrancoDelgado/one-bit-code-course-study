a900d4e000e9467c47fdb4f77cfd9539776aeefc
Ricardo Franco Delgado <rf041555@gmail.com>
2024-10-10 07:12:19 -0300

    correção na classe Poster

--- javascript-modulo-4/21-js-bookstore/Classes/Poster.js
+++ javascript-modulo-4/21-js-bookstore/Classes/Poster.js
@@ -1,6 +1,6 @@
 const Product = require("./Product");
 
-module.exports = class Poster extends Productuct {
+module.exports = class Poster extends Product {
   constructor(name, description, height, width, price, inStock = 0) {
     super(name, description, price, inStock);
     this.height = height
